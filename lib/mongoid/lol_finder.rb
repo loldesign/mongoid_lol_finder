6ee3e03da4e7ac50a5fa0fc4bc25a38229a8f8d0
Marco Singer <markaum@gmail.com>
2013-04-05 18:49:33 -0300

    just some refactories

--- lib/mongoid/lol_finder.rb
+++ lib/mongoid/lol_finder.rb
@@ -8,37 +8,41 @@ module Mongoid
       end
 
       def search(query="", options={})
-        @find_for_fields.inject(scoped) do |relation, field|
+        @find_for_fields.inject(scoped) do |relation, attr|
 
-          case way_to_search(field)
+          case way_to_search(attr)
           when :embedded
-            search_by_embedded(relation, field.to_s, query)
+            search_by_embedded(relation, attr, query)
           when :association
-            search_by_association(relation, field.to_s, query)
+            search_by_association(relation, attr, query)
           when :model
-            search_by_model(relation, field.to_s, query)
+            search_in_model(relation, attr, query)
           end
         end
       end
 
-      def search_by_association(relation, field, query)
-        association = field.to_s.classify.constantize.send('search', query).first if query.present?
-        if association.nil?
-          relation
-        else
-          relation.or({"#{field}_id" => association.id})
+      private
+
+      def klass_by(resource)
+        resource.to_s.classify.constantize
+      end
+
+      def search_by_association(relation, resource, query)
+        association = klass_by(resource).search(query).first if query.present?
+        return relation if association.nil?
+
+        relation.or({"#{resource}" => association})
+      end
+
+      def search_by_embedded(relation, resource, query)
+        klass_by(resource).fields.keys.inject(relation) do |relation, field|
+          relation.or({"#{resource}.#{field}" => query})
         end
       end
 
-      def search_by_embedded(relation, field, query)
-        field.classify.constantize.fields.keys.inject(relation) do |relation, attr|
-          relation.or({"#{field}.#{attr}" => query})
-        end
-      end
-
-      def search_by_model(relation, field, query)
-        if self.fields[field.to_s].type == Hash
-          relation.or({:"#{field}.#{query}".exists => true})
+      def search_in_model(relation, field, query)
+        if fields[field.to_s].type == Hash
+          relation.or({"#{field}.#{query}".to_sym.exists => true})
         else
           relation.or({field => /#{query}/i})
         end
